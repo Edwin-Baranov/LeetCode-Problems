6bf8237d922339defe69b0de76faa9ae9306e35c
Edwin Baranov <edwin.baranov@gmail.com>
2023-03-23 16:01:31 -0700

    Time: 45 ms, Memory: 48.5 MB - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -3,7 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             if (i > maxReach) {
                 return false;
             }
