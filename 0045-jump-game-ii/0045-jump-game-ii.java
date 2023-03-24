740f28356f08157d48db114cfae40abb34e235b4
Edwin Baranov <edwin.baranov@gmail.com>
2023-03-24 11:31:29 -0700

    Time: 1 ms (99.53%), Space: 43.2 MB (17.60%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int maxDistance = nums[0];
+        int result = 0;
+        
+        for (int i = 0, target = 0; i <= target; ++i) {
+            if (target >= nums.length - 1) break;
+            maxDistance = Math.max(maxDistance, i + nums[i]);
+            
+            if (i == target) {
+                ++result;
+                target = maxDistance;
+            }
+        }
+        return result;
+    }
+}
