eae4a6688581132f6f9cd41a11eb90b4e98a3733
Edwin Baranov <edwin.baranov@gmail.com>
2023-03-31 15:28:25 -0700

    Time: 6 ms (99.34%), Space: 40.5 MB (40.92%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.java
@@ -12,8 +12,8 @@ class Solution {
 
         //Populate the appleCount by grabing the apple state value from pizza for specified space
         //and adding both area values for the under and the right while deleting an overlap
-        for (int row = rows - 1; row >= 0; row--) {
-            for (int col = cols - 1; col >= 0; col--) {
+        for (int row = rows - 1; row >= 0; --row) {
+            for (int col = cols - 1; col >= 0; --col) {
                 int state = pizza[row].charAt(col) == 'A' ? 1 : 0;
                 int down = appleCount[row + 1][col];
                 int right = appleCount[row][col + 1];
@@ -25,22 +25,24 @@ class Solution {
         return recursion(0, 0, k - 1);
     }
 
-    private int recursion(int r, int c, int cutsLeft) {
-        if (appleCount[r][c] == 0) return 0;
+    //Record and return the number of times where the remaining piece contained apples
+    //at the specified location for both horizontal and vertical cuts with number of cuts left
+    private int recursion(int row, int col, int cutsLeft) {
+        if (appleCount[row][col] == 0) return 0;
         if (cutsLeft == 0) return 1;
-        if (dp[cutsLeft][r][c] != null) return dp[cutsLeft][r][c];
+        if (dp[cutsLeft][row][col] != null) return dp[cutsLeft][row][col];
 
         long res = 0L;
-        for (int nr = r; nr < rows; nr++) {
-            if (appleCount[r][c] - appleCount[nr][c] > 0) {
-                res += recursion(nr, c, cutsLeft - 1);
+        for (int nextRow = row; nextRow < rows; ++nextRow) {
+            if (appleCount[row][col] - appleCount[nextRow][col] > 0) {
+                res += recursion(nextRow, col, cutsLeft - 1);
             }
         }
-        for (int nc = c; nc < cols; nc++) {
-            if (appleCount[r][c] - appleCount[r][nc] > 0) {
-                res += recursion(r, nc, cutsLeft - 1);
+        for (int nextCol = col; nextCol < cols; ++nextCol) {
+            if (appleCount[row][col] - appleCount[row][nextCol] > 0) {
+                res += recursion(row, nextCol, cutsLeft - 1);
             }
         }
-        return dp[cutsLeft][r][c] = (int) (res % mod);
+        return dp[cutsLeft][row][col] = (int) (res % mod);
     }
 }
