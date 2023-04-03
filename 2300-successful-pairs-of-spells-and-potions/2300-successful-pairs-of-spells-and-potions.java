0c51788498dbd1014af3dfc8a89dc6d6c8bc6243
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-02 17:54:22 -0700

    Time: 65 ms (87.08%), Space: 62 MB (51.25%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,30 +1,35 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int n = spells.length;
-        int m = potions.length;
-        
-        int[][] sortedSpells = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            sortedSpells[i][0] = spells[i];
-            sortedSpells[i][1] = i;
-        }
-        
-        Arrays.sort(sortedSpells, (a, b) -> Integer.compare(a[0], b[0]));
         Arrays.sort(potions);
         
-        int[] answer = new int[n];
-        int rightP  = m - 1;
+        int[] answer = new int[spells.length];
         
-        for (int[] sortedSpell : sortedSpells) {
-            int spell = sortedSpell[0];
-            int index = sortedSpell[1];
+        for (int i = 0; i < spells.length; ++i) {
+            long target = (long) Math.ceil((1.0 * success) / spells[i]);
             
-            while (rightP >= 0 && (long) spell * potions[rightP] >= success) {
-                --rightP;
+            if (target > potions[potions.length - 1]) {
+                answer[i] = 0;
+                continue;
             }
             
-            answer[index] = m - (rightP + 1);
+            int search = binarySearch(potions, target);
+            answer[i] = potions.length - search;            
         }
         return answer;
+    }
+    
+    public int binarySearch(int[] arr, long target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
     }
 }
