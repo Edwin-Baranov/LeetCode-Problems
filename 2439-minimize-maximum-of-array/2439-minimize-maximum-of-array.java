4899c4dcc6d591ab3d4152ffd9ab97721413ea64
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-05 12:34:13 -0700

    Time: 9 ms (74.55%), Space: 62 MB (37.28%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minimizeArrayValue(int[] nums) {
-        long answer = 0, sum = 0;
+        int answer = 0;
+        long sum = 0;
         
         for (int i = 0; i < nums.length; ++i) {
             sum += nums[i];
-            answer = Math.max(answer, (sum + i) / (i + 1));
+            answer = Math.max(answer, (int)Math.ceil((sum) / (double)(i + 1)));
         }
         
-        return (int)answer;
+        return answer;
     }
 }
