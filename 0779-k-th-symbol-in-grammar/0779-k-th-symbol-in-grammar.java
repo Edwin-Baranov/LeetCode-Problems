909f3102bbe1e3ff764d83f8be8e7bcb2742a01c
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-11 12:55:50 -0700

    Time: 0 ms (100.00%), Space: 40 MB (12.42%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -2,11 +2,10 @@ class Solution {
     public int kthGrammar(int n, int k) {
         if (n == 1)
             return 0;
-        int halfRowLength = (int)Math.pow(2, n-2);
         
-        if (k > halfRowLength) {
+        if (k > (int)Math.pow(2, n-2)) {
             //return the inverse of parent
-            return ~kthGrammar(n-1, k-halfRowLength) & 1;
+            return ~kthGrammar(n-1, k-(int)Math.pow(2, n-2)) & 1;
         } else {
             return kthGrammar(n-1, k);
         }
