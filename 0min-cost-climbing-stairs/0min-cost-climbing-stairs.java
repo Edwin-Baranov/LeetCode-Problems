f83b5ab72b47bda80973b5dfa0282499fcbc7596
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-15 18:01:18 -0700

    Time: 1 ms, Memory: 42 MB - LeetHub

--- 0min-cost-climbing-stairs/0min-cost-climbing-stairs.java
+++ 0min-cost-climbing-stairs/0min-cost-climbing-stairs.java
@@ -1,18 +1,10 @@
 class Solution {
-    int[] cost;
-    int[] memo;
     public int minCostClimbingStairs(int[] cost) {
-        this.cost = cost;
-        this.memo = new int[cost.length+1];
-        return dp(cost.length);
-    }
-    
-    private int dp(int i) {
-        if (i <= 1) return 0;
-        if (memo[i] != 0) return memo[i];
+        int[] memo = new int[cost.length + 1];
         
-        memo[i] = Math.min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2]);
-        
-        return memo[i];
+        for (int i = 2; i < memo.length; ++i) {
+            memo[i] = Math.min(memo[i-1] + cost[i-1], memo[i-2] + cost[i-2]);
+        }
+        return memo[memo.length - 1];
     }
 }
