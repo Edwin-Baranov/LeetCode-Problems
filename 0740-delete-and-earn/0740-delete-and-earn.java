79fb1e701913aa412c20982ceb191e4c184a7db0
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-19 14:11:23 -0700

    Time: 12 ms (34.65%), Space: 43.4 MB (45.48%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,23 +1,30 @@
 class Solution {
     Map<Integer, Integer> points = new HashMap<>();
-    Map<Integer, Integer> cache = new HashMap<>();
     
     public int deleteAndEarn(int[] nums) {
-        int maxNum = 0;
+        
         for (int num : nums) {
             points.put(num, points.getOrDefault(num, 0) + num);
-            maxNum = Math.max(maxNum, num);
         }
         
-        int temp1 = 0;
-        int temp2 = points.getOrDefault(1, 0);
+        List<Integer> elements = new ArrayList<Integer>(points.keySet());
+        Collections.sort(elements);
         
-        for (int i = 2; i <= maxNum; ++i) {
-            int newMax = Math.max(temp2, temp1 + points.getOrDefault(i, 0));
-            temp1 = temp2;
-            temp2 = newMax;
+        int twoBack = 0;
+        int oneBack = points.get(elements.get(0));
+        
+        for (int i = 1; i < elements.size(); ++i) {
+            int currentElement = elements.get(i);
+            int temp = oneBack;
+            
+            if(currentElement == elements.get(i - 1) + 1) {
+                oneBack = Math.max(oneBack, twoBack + points.get(currentElement));
+            } else {
+                oneBack += points.get(currentElement);
+            }
+            twoBack = temp;
         }
         
-        return temp2;
+        return oneBack;
     }
 }
