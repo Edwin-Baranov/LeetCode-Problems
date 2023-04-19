6d2e41c7c5660b2c074dbe37d1f00fac5302babc
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-19 12:35:31 -0700

    Time: 0 ms, Memory: 39.4 MB - LeetHub

--- 000n-th-tribonacci-number/000n-th-tribonacci-number.java
+++ 000n-th-tribonacci-number/000n-th-tribonacci-number.java
@@ -7,16 +7,17 @@ class Solution {
             return 1;
         }
         
-        int[] db = new int[3];
-        db[1] = db[2] = 1;
+        int first, second, third;
+        first = 0;
+        second = third = 1;
         
         for (int i = 3; i <= n; ++i) {
-            int temp = db[0] + db[1] + db[2];
-            db[0] = db[1];
-            db[1] = db[2];
-            db[2] = temp;
+            int temp = first + second + third;
+            first = second;
+            second = third;
+            third = temp;
         }
         
-        return db[2];
+        return third;
     }
 }
