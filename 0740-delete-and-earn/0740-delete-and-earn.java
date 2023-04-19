d80a59ae6599cdd9ab7de3029653a257c376ce93
Edwin Baranov <edwin.baranov@gmail.com>
2023-04-19 13:38:38 -0700

    Time: 13 ms (28.37%), Space: 43.7 MB (31.61%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<Integer, Integer> points = new HashMap<>();
+    Map<Integer, Integer> cache = new HashMap<>();
+    
+    public int deleteAndEarn(int[] nums) {
+        int maxNum = 0;
+        for (int num : nums) {
+            points.put(num, points.getOrDefault(num, 0) + num);
+            maxNum = Math.max(maxNum, num);
+        }
+        
+        return maxPoints(maxNum);
+    }
+    
+    private int maxPoints(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        
+        if (num == 1) {
+            return points.getOrDefault(1, 0);
+        }
+        
+        if (cache.containsKey(num)) {
+            return cache.get(num);
+        }
+        
+        cache.put(num, Math.max(maxPoints(num-1), points.getOrDefault(num, 0) + maxPoints(num - 2)));
+        return cache.get(num);
+    }
+}
