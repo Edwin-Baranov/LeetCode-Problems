ae6df9e49c0541171066f44189ef47a955508319
Edwin Baranov <edwin.baranov@gmail.com>
2023-05-11 11:21:21 -0700

    Time: 5 ms (92.56%), Space: 42.2 MB (52.36%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -2,21 +2,21 @@ class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-
-        int[] dp = new int[n2 + 1];
-        int[] dpPrev = new int[n2 + 1];
-
-         for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[j] = 1 + dpPrev[j - 1];
-                } else {
-                    dp[j] = Math.max(dp[j - 1], dpPrev[j]);
+        
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        
+        for(int i = n1 - 1; i >= 0; --i) {
+            for(int j = n2 - 1; j >= 0; --j) {
+                int c1 = nums1[i];
+                int c2 = nums2[j];
+                if(c1 == c2) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                }
+                else {
+                    dp[i][j] =  Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
-            dpPrev = dp.clone();
         }
-
-        return dp[n2];
+        return dp[0][0];
     }
 }
