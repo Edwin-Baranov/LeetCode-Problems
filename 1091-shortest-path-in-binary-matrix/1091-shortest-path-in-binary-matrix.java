e58964ec19fcbf0f0e667d76ff1d5225820bc460
Edwin Baranov <edwin.baranov@gmail.com>
2023-06-01 11:18:14 -0700

    Time: 18 ms (56.61%), Space: 45.6 MB (6.05%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,53 +1,38 @@
 class Solution {
-    
-    private static final int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    
+
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
         
-        // Firstly, we need to check that the start and target cells are open.
-        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
         
-        // Set up the BFS.
-        Queue<int[]> queue = new ArrayDeque<>();
-        grid[0][0] = 1;
-        queue.add(new int[]{0, 0});
-        
-        // Carry out the BFS
-        while (!queue.isEmpty()) {
-            int[] cell = queue.remove();
-            int row = cell[0];
-            int col = cell[1];
-            int distance = grid[row][col];
-            if (row == grid.length - 1 && col == grid[0].length - 1) {
-                return distance;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        boolean[][] isVisited = new boolean[n][n];
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{0, 0});
+        isVisited[0][0] = true;
+        int count = 1;
+        while (!dq.isEmpty()) {
+            int size = dq.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = dq.poll();
+                
+                if (curr[0] == n - 1 && curr[1] == n - 1) {
+                    return count;
+                }
+                for (int[] direction : directions) {
+                    int x = curr[0] + direction[0];
+                    int y = curr[1] + direction[1];
+                    if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] == 1 || isVisited[x][y]) {
+                        continue;
+                    }
+                    dq.add(new int[]{x, y});
+                    isVisited[x][y] = true;
+                }
             }
-            for (int[] neighbour : getNeighbours(row, col, grid)) {
-                int neighbourRow = neighbour[0];
-                int neighbourCol = neighbour[1];
-                queue.add(new int[]{neighbourRow, neighbourCol});
-                grid[neighbourRow][neighbourCol] = distance + 1;
-            }
+            count++;
         }
-        
-        // The target was unreachable.
         return -1;
     }
-    
-    private List<int[]> getNeighbours(int row, int col, int[][] grid) {
-        List<int[]> neighbours = new ArrayList<>();
-        for (int i = 0; i < directions.length; i++) {
-            int newRow = row + directions[i][0];
-            int newCol = col + directions[i][1];
-            if (newRow < 0 || newCol < 0 || newRow >= grid.length 
-                    || newCol >= grid[0].length
-                    || grid[newRow][newCol] != 0) {
-                continue;    
-            }
-            neighbours.add(new int[]{newRow, newCol});
-        }
-        return neighbours; 
-    }
-    
 }
