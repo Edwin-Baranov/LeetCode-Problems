21b53bfd6d1fd1e357218e093b07e377c13baa18
Edwin Baranov <edwin.baranov@gmail.com>
2023-06-05 13:12:14 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (36.95%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,24 +1,24 @@
 class Solution {
-    // Returns the delta Y.
     int getYDiff(int[] a, int[] b) {
         return a[1] - b[1];
     }
     
-    // Returns the delta X.
     int getXDiff(int[] a, int[] b) {
         return a[0] - b[0];
     }
     
     public boolean checkStraightLine(int[][] coordinates) {
-        int deltaY = getYDiff(coordinates[1], coordinates[0]);
-        int deltaX = getXDiff(coordinates[1], coordinates[0]);
+        int[] first = coordinates[0];
+        
+        int deltaY = coordinates[1][1] - first[1];
+        int deltaX = coordinates[1][0] - first[0];
         
         for (int i = 2; i < coordinates.length; i++) {
-            // Check if the slope between points 0 and i, is the same as between 0 and 1.
-            if (deltaY * getXDiff(coordinates[i], coordinates[0])
-                != deltaX * getYDiff(coordinates[i], coordinates[0])) {
+            int newDeltaY = coordinates[i][1] - first[1];
+            int newDeltaX = coordinates[i][0] - first[0];
+            
+            if (deltaY * newDeltaX != deltaX * newDeltaY)
                 return false;
-            }
         }
         return true;
     }
