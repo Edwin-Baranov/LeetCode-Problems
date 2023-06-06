b97ec12fb7f04f93a5a2eb40b1bebc581ac98e7d
Edwin Baranov <edwin.baranov@gmail.com>
2023-06-06 09:59:01 -0700

    Time: 2 ms (81.19%), Space: 40.9 MB (91.81%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
-        int left = arr[0];
-        int right = arr[1];
-        int progression = right - left;
+        int compair = arr[1];
+        int progression = compair - arr[0];
         
         for (int i = 2; i < arr.length; ++i) {
-            if (arr[i] - right != progression)
+            if (arr[i] - compair != progression)
                 return false;
             
-            right = arr[i];
+            compair = arr[i];
         }
         return true;
     }
