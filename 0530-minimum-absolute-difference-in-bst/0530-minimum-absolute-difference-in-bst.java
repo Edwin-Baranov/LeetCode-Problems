bff49ba134020cb441505147ba245c1b01e9bd35
Edwin Baranov <edwin.baranov@gmail.com>
2023-06-14 16:58:09 -0700

    Time: 0 ms (100.00%), Space: 43.6 MB (18.45%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -14,29 +14,26 @@
  * }
  */
 class Solution {
-    // List to store the tree nodes in the inorder traversal.
-    List<Integer> inorderNodes = new ArrayList<>();
-    
+    int minDifference = Integer.MAX_VALUE;
+    // Initially, it will be null.
+    TreeNode prevNode;
+
     void inorderTraversal(TreeNode node) {
         if (node == null) {
             return;
         }
-        
+
         inorderTraversal(node.left);
-        // Store the nodes in the list.
-        inorderNodes.add(node.val);
+        // Find the difference with the previous value if it is there.
+        if (prevNode != null) {
+            minDifference = Math.min(minDifference, node.val - prevNode.val);
+        }
+        prevNode = node;
         inorderTraversal(node.right);
     }
-    
+
     int getMinimumDifference(TreeNode root) {
-       inorderTraversal(root);
-        
-        int minDifference = Integer.MAX_VALUE;
-        // Find the diff between every two consecutive values in the list.
-        for (int i = 1; i < inorderNodes.size(); i++) {
-            minDifference = Math.min(minDifference, inorderNodes.get(i) - inorderNodes.get(i-1));
-        }
-        
+        inorderTraversal(root);
         return minDifference;
     }
 };
