c069444f6d92f29e8d81c9e3177e20f3cc687f8e
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-23 15:50:16 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (21.84%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,10 +1,14 @@
 public class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int i = 0;
-        while (i < citations.length && citations[citations.length - 1 - i] > i) {
-            i++;
-        }
-        return i;
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // finding the h-index
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
     }
 }
