30e8d5b88f894bcdc34feefe88d23f24ee24da05
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-23 19:35:09 -0700

    Time: 6 ms (19.92%), Space: 44.4 MB (17.10%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,34 +1,23 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[][] sorted = new int[n][2]; //[rating, index]
-        int[] candy = new int[n];
+        int[] candies = new int[ratings.length];
         int result = 0;
         
-        for (int i = 0; i < n; i++) {
-            sorted[i][0] = ratings[i];
-            sorted[i][1] = i;
+        Arrays.fill(candies, 1);
+        
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i-1] + 1;
+            }
         }
         
-        Arrays.sort(sorted, (a, b) -> a[0] - b[0]);
-        
-        for (int[] child : sorted) {
-            int index = child[1];
-            int rating = child[0];
-            int left = 0;
-            int right = 0;
-            if (index - 1 >= 0 && ratings[index - 1] < rating) {
-                left = candy[index - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
-            
-            if (index + 1 < n && ratings[index + 1] < rating) {
-                right = candy[index + 1];
-            }
-            
-            candy[index] = Math.max(left, right) + 1;
-            result += candy[index];
         }
         
-        return result;
+        return Arrays.stream(candies).sum();
     }
 }
