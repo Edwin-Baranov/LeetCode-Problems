2fd00bfbd16edfdadaa0f15f7ca7c7680503f7e8
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-27 16:41:23 -0700

    Time: 3 ms (72.60%), Space: 43.8 MB (31.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int n = board.length;
+        Set<Character> dict = new HashSet();
         
-        return isValidRows(board) && isValidColumns(board) && isValidBoxes(board);
-    }
-    
-    private boolean isValidRows(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            Set<Character> dict = new HashSet();
-            for (int j = 0; j < board.length; j++) {
+        for (int i = 0; i < n; i++) {
+            dict.clear();
+            for (int j = 0; j < n; j++) {
                 char c = board[i][j];
                 if (c != '.' && dict.contains(c)) {
                     return false;
@@ -17,14 +14,10 @@ class Solution {
                 }
             }
         }
-        return true;
         
-    }
-    
-    private boolean isValidColumns(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            Set<Character> dict = new HashSet();
-            for (int j = 0; j < board.length; j++) {
+        for (int i = 0; i < n; i++) {
+            dict.clear();
+            for (int j = 0; j < n; j++) {
                 char c = board[j][i];
                 if (c != '.' && dict.contains(c)) {
                     return false;
@@ -33,13 +26,10 @@ class Solution {
                 }
             }
         }
-        return true;
-    }
-    
-    private boolean isValidBoxes(char[][] board) {
-        for (int i = 0; i < board.length; i += 3) {
-            for (int j = 0; j < board.length; j += 3) {
-                Set<Character> dict = new HashSet();
+        
+        for (int i = 0; i < n; i += 3) {
+            for (int j = 0; j < n; j += 3) {
+                dict.clear();
                 for (int k = i; k < i + 3; k++) {
                     for (int l = j; l < j + 3; l++) {
                         char c = board[k][l];
@@ -52,6 +42,7 @@ class Solution {
                 }
             }
         }
+        
         return true;
     }
 }
