821b1080c5a4dc898b6329172cb962500a988b62
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-27 15:08:00 -0700

    Time: 14 ms (78.94%), Space: 44.6 MB (31.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length())
-            return "";
-        
-        int[] dp = new int[128];
+        Map<Character, Integer> neededChars = new HashMap();
+        Map<Character, Integer> seenChars = new HashMap();
+        int left = 0, right = 0;
+        int minLeft = 0, minRight = 0;
+        int subLength = -1;
+        int m = s.length();
         
         for (char c : t.toCharArray()) {
-            ++dp[c];
+            neededChars.put(c, neededChars.getOrDefault(c, 0) + 1);
         }
         
-        int left = 0, right = 0;
-        int minLeft = 0;
-        int length = Integer.MAX_VALUE;
-        int counter = t.length();
-        int n = s.length();
+        int conditionCount = neededChars.size();
         
-        while(right < n) {
-            char rightChar = s.charAt(right);
-            if (dp[rightChar] > 0)
-                --counter;
+        while (right < m) {
+            char rChar = s.charAt(right);
+            seenChars.put(rChar, seenChars.getOrDefault(rChar,0) + 1);
             
-            --dp[rightChar];
+            if (neededChars.containsKey(rChar) && seenChars.get(rChar).intValue() == neededChars.get(rChar).intValue()) {
+                conditionCount--;
+            }
             
-            while (counter == 0) {
-                if (right - left < length) {
-                    length = right - left;
+            while (left <= right && conditionCount == 0) {
+                if (subLength == -1 || right - left + 1 < subLength) {
                     minLeft = left;
+                    minRight = right + 1;
+                    subLength = minRight - minLeft;
                 }
-                char leftChar = s.charAt(left);
-                ++dp[leftChar];
-                if (dp[leftChar] > 0)
-                    ++counter;
+                
+                char lChar = s.charAt(left);
+                seenChars.put(lChar, seenChars.get(lChar) - 1);
+                if (seenChars.get(lChar) < neededChars.getOrDefault(lChar, 0)) {
+                    conditionCount++;
+                }
                 ++left;
             }
             ++right;
         }
         
-        if (length == Integer.MAX_VALUE) {
-            return "";
-        }
-        
-        return s.substring(minLeft, minLeft + length + 1);
+        return subLength == -1 ? "" : s.substring(minLeft, minRight);
     }
 }
