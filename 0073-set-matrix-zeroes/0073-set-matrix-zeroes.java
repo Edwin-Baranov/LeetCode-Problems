87dd71599df96776d391428c4d07e05eba952156
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-27 19:16:54 -0700

    Time: 0 ms (100.00%), Space: 45 MB (14.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -6,6 +6,7 @@ class Solution {
         boolean firstRow = false;
         boolean firstColumn = false;
         
+        //Record if firstColumn contains any 0's
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 firstColumn = true;
@@ -13,6 +14,7 @@ class Solution {
             }
         }
         
+        //Record if firstRow contains any 0's
         for (int i = 0; i < n; i++) {
             if (matrix[0][i] == 0) {
                 firstRow = true;
@@ -20,6 +22,9 @@ class Solution {
             }
         }
         
+        //for every item except first row and column
+        //record a 0 within first row and column to indicate
+        //row and column that needs updating
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -29,9 +34,21 @@ class Solution {
             }
         }
         
+        //Iterate along first row looking for 0's
+        //Apply 0 to all elements in column when 0 is found
+        for (int i = 1; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < m; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        
+        //Iterate along first column looking for 0's
+        //Apply 0 to all elements in row when 0 is found
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
                     matrix[i][j] = 0;
                 }
             }
