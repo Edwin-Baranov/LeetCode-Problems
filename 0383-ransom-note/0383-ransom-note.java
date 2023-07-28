d1fc07ba7297dbeb454e34946aab6be29cb879c0
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-28 11:37:10 -0700

    Time: 9 ms (62.68%), Space: 44.3 MB (36.28%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,15 +3,12 @@ class Solution {
         Map<Character, Integer> ransomCount = new HashMap();
         Map<Character, Integer> magazineCount = new HashMap();
         
-        for (char c : ransomNote.toCharArray()) {
-            ransomCount.put(c, ransomCount.getOrDefault(c, 0) + 1);
-        }
-        
         for (char c : magazine.toCharArray()) {
             magazineCount.put(c, magazineCount.getOrDefault(c, 0) + 1);
         }
         
-        for (char c : ransomCount.keySet()) {
+        for (char c : ransomNote.toCharArray()) {
+            ransomCount.put(c, ransomCount.getOrDefault(c, 0) + 1);
             if (ransomCount.get(c).intValue() > magazineCount.getOrDefault(c,0).intValue()) {
                 return false;
             }
