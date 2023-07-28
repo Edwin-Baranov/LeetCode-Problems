b30ebccdf1e2d2e87f26ebca5eebfbe266630e04
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-28 11:10:56 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (92.94%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -2,53 +2,48 @@ class Solution {
     public void gameOfLife(int[][] board) {
         int m = board.length;
         int n = board[0].length;
-        int[][] newBoard = new int[m][n];
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                newBoard[i][j] = updateCell(board, i, j);
+                updateCell(board, i, j);
             }
         }
         
-        System.arraycopy(newBoard, 0, board, 0, newBoard.length);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                }
+            }
+        }
     }
     
-    private int updateCell(int[][] board, int i, int j) {
+    private void updateCell(int[][] board, int row, int column) {
         int m = board.length;
         int n = board[0].length;
         int liveNeighbors = 0;
         
-        //top
-        if (i > 0) {
-            if (j > 0) { liveNeighbors += board[i-1][j-1]; }
-            liveNeighbors += board[i-1][j];
-            if (j < n - 1) { liveNeighbors += board[i-1][j+1]; }
-        }
-        
-        //middle
-        if (j > 0) { liveNeighbors += board[i][j-1]; }
-        if (j < n - 1) { liveNeighbors += board[i][j+1]; }
-        
-        //bottom
-        if (i < m - 1) {
-            if (j > 0) { liveNeighbors += board[i+1][j-1]; }
-            liveNeighbors += board[i+1][j];
-            if (j < n - 1) { liveNeighbors += board[i+1][j+1]; }
-            
-        }
-        
-        
-        //Generate result;
-        if (board[i][j] == 0) {
-            if (liveNeighbors == 3) {
-                return 1;
-            }
-        } else {
-            if (liveNeighbors < 2 || liveNeighbors > 3) {
-                return 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                int y = row + i - 1;
+                int x = column + j - 1;
+                
+                if (y >= 0 && y < m && x >= 0 && x < n) {
+                    int node = Math.abs(board[y][x]);
+                    liveNeighbors += (node == 1 ? 1 : 0);
+                }
             }
         }
         
-        return board[i][j];
+        if (board[row][column] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+            board[row][column] = -1;
+        } else if (board[row][column] == 0 && liveNeighbors == 3) {
+            board[row][column] = 2;
+        }
     }
 }
