13a88881b47e450dbe3e97ac186f677aa1b60b89
Edwin Baranov <edwin.baranov@gmail.com>
2023-07-27 19:06:41 -0700

    Time: 1 ms (75.67%), Space: 44.8 MB (28.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,23 +2,51 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        Set<Integer> rows = new HashSet();
-        Set<Integer> columns = new HashSet();
+        
+        boolean firstRow = false;
+        boolean firstColumn = false;
         
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            if (matrix[i][0] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    columns.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rows.contains(i) || columns.contains(j)) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        
+        if (firstColumn) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+        
+        if (firstRow) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
             }
         }
     }
