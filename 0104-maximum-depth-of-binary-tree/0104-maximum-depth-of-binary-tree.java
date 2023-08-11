43b412219e48da4a08b2f07600d329f536662d04
Edwin Baranov <edwin.baranov@gmail.com>
2023-08-11 14:03:35 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (89.56%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,27 +14,13 @@
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        
-        Stack<TreeNode> runnerStack = new Stack<>(), collectorStack = new Stack<>();
-        runnerStack.push(root);
-        
-        int result = 0;
-        
-        while (!runnerStack.isEmpty()) {
-            TreeNode popped = runnerStack.pop();
-            if (popped.left != null) collectorStack.push(popped.left);
-            if (popped.right != null) collectorStack.push(popped.right);
-            
-            if (runnerStack.isEmpty()) {
-                Stack<TreeNode> temp = runnerStack;
-                runnerStack = collectorStack;
-                collectorStack = temp;
-                result++;
-            }
-        }
-        return result;
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      int left_height = maxDepth(root.left);
+      int right_height = maxDepth(root.right);
+      return java.lang.Math.max(left_height, right_height) + 1;
     }
+  }
 }
