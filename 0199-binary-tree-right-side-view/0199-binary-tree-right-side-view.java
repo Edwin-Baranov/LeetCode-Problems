94fd8478133acd4b4570bb6879c7f4f6b7c36290
Edwin Baranov <edwin.baranov@gmail.com>
2023-08-12 16:44:15 -0700

    Time: 0 ms (100.00%), Space: 41.4 MB (52.59%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result;
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        result = new ArrayList<>();
+        
+        lookupRight(root, 0);
+        return result;
+    }
+    
+    private void lookupRight(TreeNode root, int depth) {
+        if (root == null)
+            return;
+        
+        if (result.size() == depth) {
+            result.add(root.val);
+        } else {
+            result.set(depth, root.val);
+        }
+        
+        lookupRight(root.left, depth + 1);
+        lookupRight(root.right, depth + 1);
+    }
+}
