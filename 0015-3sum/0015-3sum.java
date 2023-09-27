8be2c37f20e80de0491f1c26c46eeb17abbab04c
Edwin Baranov <edwin.baranov@gmail.com>
2023-09-26 17:35:04 -0700

    Time: 24 ms (97.96%), Space: 50.4 MB (81.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        
-        //Sort array so we can start with the negative numbers
         Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
         
-        //loop only using non positive numbers
-        for (int i = 0; i < n && nums[i] <= 0; i++) {
-            if (i == 0 || nums[i-1] != nums[i]) {
-                Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < n-2 && nums[i] <= 0; ++i) {
+            if (i != 0 && nums[i-1] == nums[i])
+                continue;
+            
+            int left = i+1, right = n-1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 
-                for (int j = i + 1; j < n; j++) {
-                    //skip over duplicate numbers
-                    
-                    int needed = -(nums[i] + nums[j]);
-                    
-                    //once a needed value is found within the set create a list
-                    if (seen.contains(needed)) {
-                        result.add(Arrays.asList(nums[i], nums[j], needed));
-                        //if combination found skip over nums[j] duplicates
-                        while (j + 1 < n && nums[j] == nums[j+1])
-                            j++;
-                    }
-                    seen.add(nums[j]);
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-        
         return result;
     }
 }
