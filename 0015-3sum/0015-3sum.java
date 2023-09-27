8fa90cf055ee93cc104a01428bf4d4761281a4a3
Edwin Baranov <edwin.baranov@gmail.com>
2023-09-26 17:06:59 -0700

    Time: 304 ms (27.34%), Space: 50.9 MB (47.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        
         int n = nums.length;
         
-        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) {
-            int left = i + 1, right = n - 1;
+        //Sort array so we can start with the negative numbers
+        Arrays.sort(nums);
+        
+        //loop only using non positive numbers
+        for (int i = 0; i < n && nums[i] <= 0; i++) {
             if (i == 0 || nums[i-1] != nums[i]) {
-                while (left < right) {
-                    int sum = nums[i] + nums[left] + nums[right];
-
-                    if (sum < 0) {
-                        left++;
-                    } else if (sum > 0) {
-                        right--;
-                    } else {
-                        result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
-                        while (left < right && nums[left] == nums[left - 1])
-                            left++;
+                Set<Integer> seen = new HashSet<>();
+                
+                for (int j = i + 1; j < n; j++) {
+                    //skip over duplicate numbers
+                    
+                    int needed = -(nums[i] + nums[j]);
+                    
+                    //once a needed value is found within the set create a list
+                    if (seen.contains(needed)) {
+                        result.add(Arrays.asList(nums[i], nums[j], needed));
+                        //if combination found skip over nums[j] duplicates
+                        while (j + 1 < n && nums[j] == nums[j+1])
+                            j++;
                     }
+                    seen.add(nums[j]);
                 }
             }
         }
+        
         return result;
     }
 }
