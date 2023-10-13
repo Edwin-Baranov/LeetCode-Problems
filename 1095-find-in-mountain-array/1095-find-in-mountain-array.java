fedf3cc8feb7863f8ada4efbe2b9b2622d291149
Edwin Baranov <edwin.baranov@gmail.com>
2023-10-12 18:41:36 -0700

    Time: 0 ms (100.00%), Space: 43 MB (69.30%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -9,38 +9,43 @@
  
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-        return binarySearch(target, 0, mountainArr.length() - 1, mountainArr);
+        Map<Integer, Integer> cache = new HashMap<>();
+        return binarySearch(target, 0, mountainArr.length() - 1, mountainArr, cache);
     }
     
-    private int binarySearch(int target, int left, int right, MountainArray mountainArr) {
+    private int binarySearch(int target, int left, int right, MountainArray mountainArr, Map<Integer, Integer> cache) {
         if (right < left)
             return -1;
         
         int mid = (right - left) / 2 + left;
         
-        int current = mountainArr.get(mid);
+        int current = cache.getOrDefault(mid, mountainArr.get(mid));
+        cache.put(mid, current);
+        
         boolean goingUp = true;
         
         if (mid > 0) {
-            goingUp = mountainArr.get(mid-1) < current;
+            int neighbor = cache.getOrDefault(mid-1, mountainArr.get(mid-1));
+            cache.put(mid-1, neighbor);
+            goingUp = neighbor < current;
         }
         
         if (target > current) {
             if (goingUp) {
-                return binarySearch(target, mid + 1, right, mountainArr);
+                return binarySearch(target, mid + 1, right, mountainArr, cache);
             } else {
-                return binarySearch(target, left, mid - 1, mountainArr);
+                return binarySearch(target, left, mid - 1, mountainArr, cache);
             }
             
         } else if (target < current) {
-            int lower = binarySearch(target, left, mid - 1, mountainArr);
+            int lower = binarySearch(target, left, mid - 1, mountainArr, cache);
             if (lower == -1)
-                return binarySearch(target, mid + 1, right, mountainArr);
+                return binarySearch(target, mid + 1, right, mountainArr, cache);
                 
             return lower;
         }
         
-        int lower = binarySearch(target, left, mid - 1, mountainArr);
+        int lower = binarySearch(target, left, mid - 1, mountainArr, cache);
         if (lower == -1)
             return mid;
         
