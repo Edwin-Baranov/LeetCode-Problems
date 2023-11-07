714bd8fa40b295016ffecb1b042f968073d98935
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-07 15:28:48 -0800

    Time: 0 ms (100.00%), Space: 44.1 MB (41.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        fillPixels(image, sr, sc, color, image[sr][sc]);
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
         return image;
     }
-    
-    private void fillPixels(int[][] image, int sr, int sc, int color, int selected) {
-        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] == color)
-            return;
-        
-        if (image[sr][sc] == selected) {
-            image[sr][sc] = color;
-            
-            fillPixels(image, sr + 1, sc, color, selected);
-            fillPixels(image, sr - 1, sc, color, selected);
-            fillPixels(image, sr, sc + 1, color, selected);
-            fillPixels(image, sr, sc - 1, color, selected);
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r > 0) dfs(image, r-1, c, color, newColor);
+            if (c > 0) dfs(image, r, c-1, color, newColor);
+            if (r+1 < image.length) dfs(image, r+1, c, color, newColor);
+            if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
         }
     }
 }
