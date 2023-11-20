eee0b7a8fb4727b61218d72b65495819305c6b4f
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-19 22:31:26 -0800

    Time: 121 ms (6.06%), Space: 66.6 MB (22.54%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -4,6 +4,12 @@ class Solution {
         Map<Character, Integer> timeNeeded = new HashMap<>();
         Map<Character, Boolean> found = new HashMap<>();
         Map<Character, Integer> lastIndex = new HashMap<>();
+        int[] prefixSum = new int[travel.length + 1];
+        
+        prefixSum[1] = travel[0];
+        for (int i = 1; i < travel.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + travel[i];
+        }
         
         for (int i = 0; i < garbage.length; i++) {
             for (char c : garbage[i].toCharArray()) {
@@ -11,9 +17,8 @@ class Solution {
                 
                 if (!found.getOrDefault(c, false)) {
                     found.put(c, true);
-                    for (int j = lastIndex.getOrDefault(c, 0); j < i; j++) {
-                        timeNeeded.put(c, timeNeeded.get(c) + travel[j]);
-                    }
+                    
+                    timeNeeded.put(c, timeNeeded.get(c) - prefixSum[lastIndex.getOrDefault(c, 0)] + prefixSum[i]);
                     lastIndex.put(c, i);
                 }
             }
