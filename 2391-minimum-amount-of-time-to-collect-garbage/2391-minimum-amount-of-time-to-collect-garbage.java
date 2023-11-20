d26e90b21ae74ddd0fb2acca0541bfdab1566e30
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-19 22:42:55 -0800

    Time: 74 ms (14.96%), Space: 65.9 MB (29.36%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,28 +1,29 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        //M, P, G
-        Map<Character, Integer> timeNeeded = new HashMap<>();
-        Map<Character, Integer> lastIndex = new HashMap<>();
+        Map<Character, Integer>garbageLastPos = new HashMap<Character, Integer>();
+        Map<Character, Integer>garbageCount = new HashMap<Character, Integer>();
         int[] prefixSum = new int[travel.length + 1];
         
         prefixSum[1] = travel[0];
         for (int i = 1; i < travel.length; i++) {
             prefixSum[i + 1] = prefixSum[i] + travel[i];
         }
-        
+
         for (int i = 0; i < garbage.length; i++) {
             for (char c : garbage[i].toCharArray()) {
-                lastIndex.put(c, i);
-                timeNeeded.put(c, timeNeeded.getOrDefault(c, 0) + 1);
+                garbageLastPos.put(c, i);
+                garbageCount.put(c, garbageCount.getOrDefault(c, 0) + 1);
             }
         }
-        
-        int result = 0;
-        
-        for (char c : "MPG".toCharArray()) {
-            result += timeNeeded.getOrDefault(c, 0) + prefixSum[lastIndex.getOrDefault(c, 0)];
+
+        String garbageTypes = "MPG";
+        int ans = 0;
+        for (char c : garbageTypes.toCharArray()) {
+            if (garbageCount.containsKey(c)) {
+                ans += prefixSum[garbageLastPos.get(c)] + garbageCount.get(c);
+            }
         }
-        
-        return result;
+
+        return ans;
     }
 }
