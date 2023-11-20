c3c54a789bf08ff06712722096ff466b89316261
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-19 22:52:34 -0800

    Time: 3 ms (96.97%), Space: 61.2 MB (69.32%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,29 +1,29 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        Map<Character, Integer>garbageLastPos = new HashMap<Character, Integer>();
-        Map<Character, Integer>garbageCount = new HashMap<Character, Integer>();
-        int[] prefixSum = new int[travel.length + 1];
+        int totalTime = 0;
+        int len = garbage.length;
         
-        prefixSum[1] = travel[0];
-        for (int i = 1; i < travel.length; i++) {
-            prefixSum[i + 1] = prefixSum[i] + travel[i];
+        totalTime += garbage[0].length();
+        
+        for(int i = 1; i < len; i++){
+            totalTime += garbage[i].length();
+            totalTime += 3 * travel[i-1]; 
         }
-
-        for (int i = 0; i < garbage.length; i++) {
-            for (char c : garbage[i].toCharArray()) {
-                garbageLastPos.put(c, i);
-                garbageCount.put(c, garbageCount.getOrDefault(c, 0) + 1);
-            }
+        
+        boolean foundG = garbage[len-1].contains("G");
+        boolean foundP = garbage[len-1].contains("P");
+        boolean foundM = garbage[len-1].contains("M");
+        for(int i = len-1; i>=1; i--){
+            if(!foundG) foundG = garbage[i].contains("G");
+            if(!foundP) foundP = garbage[i].contains("P");
+            if(!foundM) foundM = garbage[i].contains("M");
+            
+            if(foundP && foundG && foundM) break;
+            
+            if(!foundG) totalTime-=travel[i-1];
+            if(!foundP) totalTime-=travel[i-1];
+            if(!foundM) totalTime-=travel[i-1];
         }
-
-        String garbageTypes = "MPG";
-        int ans = 0;
-        for (char c : garbageTypes.toCharArray()) {
-            if (garbageCount.containsKey(c)) {
-                ans += prefixSum[garbageLastPos.get(c)] + garbageCount.get(c);
-            }
-        }
-
-        return ans;
+        return totalTime;
     }
 }
