be964ba5c9c4cb24cafcf1b544d5977269dbe5a1
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-27 11:08:02 -0800

    Time: 14 ms (93.21%), Space: 44.4 MB (17.66%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,35 +1,33 @@
 class Solution {
-    int[][] memory;
-    int[][] moves = new int[][]{{4,6},{6,8}, {7,9}, {4,8}, {0,3,9}, {}, {0,1,7}, {2,6}, {1,3}, {2,4}};
-    int mod = (int) 1E9 + 7;
     public int knightDialer(int n) {
-        memory = new int[10][n+1];
-        int result = 0;
-        for (int i = 0; i <= 9; i++) {
-            if (i == 5 && n != 1) continue;
+        int[][] moves = new int[][]{{4,6},{6,8}, {7,9}, {4,8}, {0,3,9}, {}, {0,1,7}, {2,6}, {1,3}, {2,4}};
+        int mod = (int) 1E9 + 7;
+        
+        int[] memory;
+        int[] prevMemory = new int[10];
+        Arrays.fill(prevMemory, 1);
+        
+        for (int i = 1; i < n; i++) {
+            memory = new int[10];
             
-            result += validNumbers(i, n);
+            for (int digit = 0; digit <= 9; digit++) {
+                int count = 0;
+                for (int nextMove : moves[digit]) {
+                    count += prevMemory[nextMove];
+                    count %= mod;
+                }
+                memory[digit] = count;
+            }
+            
+            prevMemory = memory;
+        }
+        
+        int result = 0;
+        
+        for (int digit = 0; digit <= 9; digit++) {
+            result += prevMemory[digit];
             result %= mod;
-            
         }
-        
-        return result;
-    }
-    
-    private int validNumbers(int keyNumber, int hops) {
-        if (hops == 1) return 1;
-        
-        int mem = memory[keyNumber][hops];
-        if (mem != 0) return mem;
-        
-        int result = 0;
-        
-        for (int next : moves[keyNumber]) {
-            result += validNumbers(next, hops - 1);
-            result %= mod;            
-        }
-        
-        memory[keyNumber][hops] = result;
         
         return result;
     }
