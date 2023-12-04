5989d03a351f157f471ac4b3e301349af1510846
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-04 12:03:01 -0800

    Time: 1 ms (91.47%), Space: 41.1 MB (60.14%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -3,35 +3,26 @@ class Solution {
         int windowSize = 3;
         
         char[] chars = num.toCharArray();
-        char currentChar = chars[0];
+        char currentDigit = chars[0];
         int count = 1;
         char result = '\0';
-        
+
         for (int i = 1; i < chars.length; i++) {
-            if (chars[i] != currentChar || count == windowSize) {
+            if (chars[i] != currentDigit) {
                 if (i > chars.length - windowSize)
                     break;
                 
-                currentChar = chars[i];
+                currentDigit = chars[i];
                 count = 1;
             } else {
                 count++;
-                
-                if (count == windowSize) {
-                    if (currentChar > result)
-                        result = currentChar;
-                }
+            }
+
+            if (count == windowSize && currentDigit > result) {
+                result = currentDigit;
             }
         }
-        
-        if (result == '\0') {
-            return "";
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < windowSize; i++) {
-                sb.append(result);
-            }
-            return sb.toString();
-        }
+
+        return result == '\0' ? "" : String.valueOf(result).repeat(windowSize);
     }
 }
