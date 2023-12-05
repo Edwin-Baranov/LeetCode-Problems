5d03062a360a181065e14893a9af261941db1f3a
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-04 19:22:12 -0800

    Time: 0 ms (100.00%), Space: 38.6 MB (99.20%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,14 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        if (n == 1)
-            return 0;
-        
-        int half = n/2;
-        
-        if (n % 2 == 0) {
-            return half + numberOfMatches(half);
-        } else {
-            return half + 1 + numberOfMatches(half);
-        }
+        return n-1;
     }
 }
