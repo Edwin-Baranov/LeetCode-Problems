352c30b6038ed82c67ba92592682997d51a53a08
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-12 15:51:11 -0800

    Time: 2 ms (95.56%), Space: 42 MB (39.93%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.java
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.java
@@ -1,24 +1,47 @@
 class Solution {
-    public int minimumMoves(int[][] grid) {
-        return dfs(grid, 0, 0);
-    }
+    int result = Integer.MAX_VALUE;
     
-    private int dfs(int[][] grid, int row, int colum) {
-        if (row == 3) return 0;
-        if (colum == 3) return dfs(grid, row + 1, 0);
+    public int minimumMoves(int[][] grid) {
+
+        List<int[]> emptyCells = new ArrayList<>();
+        List<int[]> extraCells = new ArrayList<>();
         
-        int result = Integer.MAX_VALUE;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(grid[i][j] > 0) {
-                    grid[i][j]--;
-                    int s = Math.abs(i - row) + Math.abs(j - colum) + dfs(grid, row, colum + 1);
-                    result = Math.min(result, s);
-                    grid[i][j]++;
-                }
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(grid[i][j] == 0)
+                    emptyCells.add(new int[]{i, j});
+                
+                //Store extra cell's cord and extra count
+                if(grid[i][j] > 1)
+                    extraCells.add(new int[]{i, j, grid[i][j] - 1});
             }
         }
-        
+
+        solve(emptyCells, extraCells, 0, 0);
         return result;
+    }
+    
+    private void solve(List<int[]> emptyCells, List<int[]> extraCells, int emptyIndex, int moves){
+        //If we have no more cells to fill, update result to be min number of moves
+        if(emptyIndex == emptyCells.size()) {
+            result = Math.min(result, moves);
+            return;
+        }
+        
+        int[] emptyCell = emptyCells.get(emptyIndex);
+        
+        for(int i = 0; i < extraCells.size(); i++){
+            int[] extraCell = extraCells.get(i);
+            
+            //if extraCell still has extra stones to spare
+            if(extraCell[2] > 0){
+                extraCell[2]--;
+                int displacement = Math.abs(extraCell[0] - emptyCell[0]) + Math.abs(extraCell[1] - emptyCell[1]);
+                
+                solve(emptyCells, extraCells, emptyIndex + 1, moves + displacement);
+                
+                extraCell[2]++;
+            }
+        }
     }
 }
