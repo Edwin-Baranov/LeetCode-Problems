d1c98cfc5ca4bd67d0e2b8233419c2ef8d4bc580
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-12 12:16:47 -0800

    Time: 16 ms (76.97%), Space: 41.8 MB (65.00%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int[] ans = new int[]{0, 0};
+        int left = 0, right = 0;
         
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                ans[0] = i;
-                ans[1] = i + 1;
+        for (int i = 0; i < s.length(); i++) {
+            int oddLength = expand(i, i, s);
+            if (oddLength > right - left + 1) {
+                int dist = oddLength / 2;
+                left = i - dist;
+                right = i + dist;
+            }
+            
+            int evenLength = expand(i, i + 1, s);
+            if (evenLength > right - left + 1) {
+                int dist = (evenLength / 2) - 1;
+                left = i - dist;
+                right = i + 1 + dist;
             }
         }
         
-        for (int diff = 2; diff < n; diff++) {
-            for (int i = 0; i < n - diff; i++) {
-                int j = i + diff;
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    ans[0] = i;
-                    ans[1] = j;
-                }
-            }
-        }
+        return s.substring(left, right + 1);
+    }
+    
+    private int expand(int i, int j, String s) {
+        int left = i;
+        int right = j;
         
-        int i = ans[0];
-        int j = ans[1];
-        return s.substring(i, j + 1);
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
     }
 }
