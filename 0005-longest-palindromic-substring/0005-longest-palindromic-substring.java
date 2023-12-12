0645c283736b17e6d3c0261da871737a81b3f1e8
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-12 12:00:05 -0800

    Time: 113 ms (39.62%), Space: 45.6 MB (13.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,58 +1,34 @@
 class Solution {
     public String longestPalindrome(String s) {
-        Map<Character, List<Integer>> indexes = new HashMap<>();
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] ans = new int[]{0, 0};
         
-        int left = 0, right = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
         
-        char[] chars = s.toCharArray();
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
+            }
+        }
         
-        addCharIndexes(indexes, chars);
-        
-        for (int i = 0; i < chars.length; i++) {
-            List<Integer> indexList = indexes.get(chars[i]);
-            
-            for (Integer index : indexList) {
-                if (index - i <= right - left)
-                    break;
-                
-                if(isPalindrome(chars, i, index)) {
-                    left = i;
-                    right = index;
-                    break;
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
                 }
             }
         }
         
-        return s.substring(left, right + 1);
-        
-        
-    }
-    
-    private boolean isPalindrome(char[] chars, int start, int end) {
-        
-        while(start < end) {
-            if (chars[start] != chars[end])
-                return false;
-            
-            start++;
-            end--;
-        }
-        
-        return true;
-    }
-    
-    private void addCharIndexes(Map<Character, List<Integer>> indexes, char[] chars) {
-        
-        for (int i = 0; i < chars.length; i++) {
-            char c = chars[i];
-            List<Integer> indexList = indexes.getOrDefault(c, new LinkedList<Integer>());
-            indexList.add(i);
-            
-            indexes.put(c, indexList);
-        }
-        
-        for (List<Integer> indexList : indexes.values()) {
-            Collections.sort(indexList, Collections.reverseOrder());
-        }
+        int i = ans[0];
+        int j = ans[1];
+        return s.substring(i, j + 1);
     }
 }
