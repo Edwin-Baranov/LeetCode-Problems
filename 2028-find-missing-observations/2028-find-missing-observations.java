ef0c1fc946e3d0868c0df652fe30542a809058e3
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-12 19:05:20 -0800

    Time: 4 ms (71.77%), Space: 57.6 MB (61.24%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.java
+++ 2028-find-missing-observations/2028-find-missing-observations.java
@@ -1,31 +1,29 @@
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-        int totalSum = mean * (n + rolls.length);
         int knownSum = 0;
         
         for (int val : rolls)
             knownSum += val;
         
-        int missingSum = totalSum - knownSum;
-        double startingVal = (double) missingSum / n;
-        double remainderSum = Math.round((startingVal % 1) * n);
+        int missingSum = mean * (n + rolls.length) - knownSum;
         
-        if (startingVal > 6 || startingVal < 1 || (remainderSum + (int) startingVal * n) != missingSum)
+        if (missingSum < n || missingSum > n * 6 || missingSum < 1)
             return new int[0];
         
         int[] result = new int[n];
+        int startingVal = missingSum / n;
+        int remainingVal = missingSum % n;
         
         for (int i = 0; i < n; i++) {
-            result[i] = (int) startingVal;
+            result[i] = startingVal;
         }
         
-        int i = 0;
-        while (remainderSum > 0) {
-            result[i]++;
-            remainderSum--;
+        int index = 0;
+        for (int i = 0; i < remainingVal; i++) {
+            result[index]++;
             
-            if (result[i] == 6)
-                i++;
+            if (result[index] == 6)
+                index++;
         }
         
         return result;
