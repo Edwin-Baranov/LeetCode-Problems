3ab694f1294ce418275cd03e9e4493188c06d83d
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-28 09:59:23 -0800

    Time: 909 ms (6.52%), Space: 57.9 MB (26.09%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>();
+    private Set<Integer> add = Set.of(1, 9, 99);
+    
+    public int getLengthOfOptimalCompression(String s, int k) {
+        return dp(s, 0, (char) ('a' + 26), 0, k);
+    }
+    
+    private int dp(String s, int index, char lastChar, int lastCharCount, int k) {
+        if (k < 0)
+            return Integer.MAX_VALUE / 2;
+        
+        if (index == s.length())
+            return 0;
+        
+        int key = index * 101 * 27 * 101 + (lastChar - 'a') * 101 * 101 + lastCharCount * 101 + k;
+        
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        
+        int keepChar;
+        int deleteChar = dp(s, index + 1, lastChar, lastCharCount, k - 1);
+        
+        if (s.charAt(index) == lastChar) {
+            keepChar = dp(s, index + 1, lastChar, lastCharCount + 1, k) + (add.contains(lastCharCount) ? 1 : 0);
+        } else {
+            keepChar = dp(s, index + 1, s.charAt(index), 1, k) + 1;
+        }
+        
+        int result = Math.min(keepChar, deleteChar);
+        memo.put(key, result);
+        
+        return result;
+    }
+}
