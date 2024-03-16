af7ebbf4d2a7032410d7071ec0ea8f97440b1ba8
Edwin Baranov <edwin.baranov@gmail.com>
2024-03-16 12:22:12 -0700

    Time: 0 ms (100.00%), Space: 2.2 MB (33.33%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.go
@@ -0,0 +1,34 @@
+func maxRepeating(sequence string, word string) int {
+    wordLen := len(word)
+    lastI := len(sequence) - wordLen + 1
+    maxSequence := 0
+    count := 0
+    mem := 0
+    
+    for i := 0; i < lastI; {
+        if (containsWord(sequence, word, i)) {
+            mem = i
+            count++
+            i += wordLen
+        } else {
+            if (count > 0) {
+                i = mem + 1
+            } else {
+                i++
+            }
+            count = 0
+        }
+        maxSequence = max(maxSequence, count)
+    }
+    return maxSequence
+}
+
+func containsWord(sequence string, word string, start int) bool {
+    for i := 0; i < len(word); i++ {
+        if (sequence[i + start] != word[i]) {
+            return false
+        }
+    }
+    
+    return true
+}
